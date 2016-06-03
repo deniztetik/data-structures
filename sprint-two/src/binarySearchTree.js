fb4a2c497cf091ce99a852643dd028d92da451e1
MKS-students <mks.students@hackreactor.com>
2016-06-03 14:12:33 -0400

    completed binarySearch.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -3,8 +3,8 @@ var BinarySearchTree = function(value){
 
   node.value = value
 
-  node.left = null
   node.right = null
+  node.left = null
 
   return node
 
@@ -16,25 +16,49 @@ bst_prototype.insert = function(item){
 
   var node = BinarySearchTree(item)
 
-  for(var n in bst){
-
-    if(node.value > n.value && node.right === null){
-
+  function recurseInsert(n) {
+    if (n.right === null && item > n.value) {
+      n.right = node;
+    } else if (n.left === null && item < n.value) {
+      n.left = node;
+    } else if (item > n.value) {
+      //go right
+      recurseInsert(n.right);
+    } else if (item < n.value) {
+      //go left
+      recurseInsert(n.left);
     }
-
   }
 
+  recurseInsert(this);
 }
 
 
 bst_prototype.contains = function(item){
+  var result = false
 
+  function recurseContains(n) {
+    if (item === n.value) result = true;
+    else if (n.right !== null && item > n.value) {
+      return recurseContains(n.right);
+    } else if (n.left !== null && item < n.value) {
+      return recurseContains(n.left);
+    }
+  }
 
+  recurseContains(this);
+  return result;
 }
 
-bst_prototype.depthFirstLog = function(item){
+bst_prototype.depthFirstLog = function(cb){
 
+  function recurseDFL(n) {
+    cb(n.value);
+    if (n.left !== null) recurseDFL(n.left);
+    if (n.right !== null) recurseDFL(n.right);
+  }
 
+  recurseDFL(this);
 }
 
 
