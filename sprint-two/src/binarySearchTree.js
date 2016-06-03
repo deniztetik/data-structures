733a0de502da884eaa075b59b3e316663980f5b2
MKS-students <mks.students@hackreactor.com>
2016-06-03 12:33:42 -0400

    finished set and graph

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,41 @@
 var BinarySearchTree = function(value){
+  var node = Object.create(bst_prototype)
+
+  node.value = value
+
+  node.left = null
+  node.right = null
+
+  return node
+
 };
+
+var bst_prototype = {};
+
+bst_prototype.insert = function(item){
+
+  var node = BinarySearchTree(item)
+
+  for(var n in bst){
+
+    if(node.value > n.value && node.right === null){
+
+    }
+
+  }
+
+}
+
+
+bst_prototype.contains = function(item){
+
+
+}
+
+bst_prototype.depthFirstLog = function(item){
+
+
+}
 
 
 /*
--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -14,19 +14,45 @@ Graph.prototype.contains = function(node){
 };
 
 Graph.prototype.removeNode = function(node){
-  this.nodes.slice
+  var index_node = this.nodes.indexOf(node)
+  this.nodes.splice(index_node,1)
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  for (var i = 0; i < this.edges.length; i++) {
+    var edge = this.edges[i];
+    if (edge[0] === fromNode && edge[1] === toNode) return true;
+  }
+  return false;
+
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+
+  this.edges.push([fromNode,toNode])
+  this.edges.push([toNode,fromNode])
+
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  var index;
+  console.log(this.edges[0],this.edges[1])
+  for (var i = 0; i < this.edges.length; i++) {
+    var edge = this.edges[i];
+    if (edge[0] === fromNode && edge[1] === toNode){
+          index = i
+    }
+  }
+
+  this.edges.splice(index,1)
+  console.log(this.edges[0],this.edges[1])
+
 };
 
 Graph.prototype.forEachNode = function(cb){
+  for (var i = 0; i < this.nodes.length; i++) {
+    cb(this.nodes[i]);
+  }
 };
 
 /*
